2c34d7a3b9d5e1cf8f9754b6bbfc617d2c704623
George Moschovitis <george.moschovitis@gmail.com>
2019-07-11 12:22:04 +0300

    web/app: make documents-stats responsive

--- web/app/src/components/documents-stats.tsx
+++ web/app/src/components/documents-stats.tsx
@@ -15,7 +15,7 @@ export const DocumentsStats: React.SFC<Props> = (props: Props): JSX.Element => {
 
     return (
         <div className="documents-stats">
-            <h2>{documentsCount} {documentsCount == 1 ? "document" : "documents"}</h2>
+            <h2>{documentsCount} {documentsCount === 1 ? "document" : "documents"}</h2>
             <div>Total size: {formatBytes(totalSize)}</div>
         </div>
     )

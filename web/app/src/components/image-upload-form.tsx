7b3b4061b80b7cc03e78c613548f8371f810f0bf
George Moschovitis <george.moschovitis@gmail.com>
2019-07-11 09:32:24 +0300

    web/app: improve image upload form

--- web/app/src/components/image-upload-form.tsx
+++ web/app/src/components/image-upload-form.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./image-upload-form.scss";
 
 interface Props {
     readonly method?: string
@@ -16,15 +17,22 @@ export class ImageUploadForm extends React.Component<Props> {
 
         return (
             <form
+                className="image-upload-form"
                 encType="multipart/form-data"
                 action={uploadURL}
                 method="post"
                 onSubmit={this.didSubmit}
             >
+                <h1>Upload a document</h1>
                 <p>
-                    <input type="file" name="docfile" accept={accept} ref={this.fileInputRef} />
+                    <label>Document Title</label>
+                    <input type="text" name="title" />
                 </p>
                 <p>
+                    <label>Document File</label>
+                    <input type="file" name="docfile" accept={accept} ref={this.fileInputRef} />
+                </p>
+                <p className="image-upload-form__actions">
                     <input type="submit" value="Upload" />
                 </p>
             </form>

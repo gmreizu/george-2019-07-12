9b8685a7790f2e3191ac66ec27d6ac5d6915bba9
George Moschovitis <george.moschovitis@gmail.com>
2019-07-16 15:40:14 +0300

    add snapshot-test for DocumentCard

--- web/app/src/components/document-card.test.tsx
+++ web/app/src/components/document-card.test.tsx
@@ -1,11 +1,22 @@
 import { shallow } from "enzyme";
 import React from "react";
+import renderer from "react-test-renderer";
 import { Document } from "../document";
 import { DocumentCard } from "./document-card";
 
-it("renders without crashing", () => {
-    const document = new Document("1", "Doc 1", "/uploads/doc1.jpg", 123)
-    const wrapper = shallow(<DocumentCard document={document} />)
-    expect(wrapper.contains("Doc 1")).toEqual(true)
-    expect(wrapper.contains(<div>123 Bytes</div>)).toEqual(true)
-});
+describe("DocumentCard", () => {
+    it("renders without crashing", () => {
+        const document = new Document("1", "Doc 1", "/uploads/doc1.jpg", 123)
+        const wrapper = shallow(<DocumentCard document={document} />)
+        expect(wrapper.contains("Doc 1")).toEqual(true)
+        expect(wrapper.contains(<div>123 Bytes</div>)).toEqual(true)
+    })
+
+    it("renders correctly", () => {
+        const document = new Document("1", "Doc 1", "/uploads/doc1.jpg", 123)
+        const tree = renderer
+            .create(<DocumentCard document={document} />)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    });
+})
